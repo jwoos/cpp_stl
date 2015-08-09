c91a955715dadb32c1317a27b4d119a4aeeeba87
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-08-08 22:28:29 -0400

    passing code, work on own

--- whenitrainsitpours/solution.py
+++ whenitrainsitpours/solution.py
@@ -1,15 +1,27 @@
 def answer(heights):
-    heights_array = [0]
+    heights_array = []
     water = 0
     max_left = 0
     max_right = 0
 
     for height in heights:
-        
+
         next_index = heights.index(height) + 1
         before_index = heights.index(height) - 1
-        
 
-        if height > heights[next_index]:
-            return "nothing"
-        elif height < heights[next_index]:
+        if heights[before_index] <= height <= heights[next_index]:
+            heights_array.append(height)
+
+        elif heights[before_index] >= height >= heights[next_index]:
+            max_left = height
+            heights_array.append(height)
+
+        elif heights[before_index] < height and heights[next_index] < height:
+            max_right = height
+            right_to_left = height
+
+        elif height < heights[before_index] and height < heights[next_index]:
+            if heights[before_index] > heights[next_index]:
+                water += heights[before_index] - height
+            else:
+                water += heights[next_index] - height
