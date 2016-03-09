978cffc4de27ece9182051dd92bc261e03d57fb7
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-03-09 20:17:55 +0000

    Returns for unix time

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -10,7 +12,41 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/:date', function(req, res, next) {
-	res.send(req.params.date);
+	let date = req.params.date;
+	
+	let toSend = {
+		unix: null,
+		natural: null
+	};
+	
+	let monthMap = {
+		0: 'January',
+		1: 'February',
+		2: 'March',
+		3: 'April',
+		4: 'May',
+		5: 'June',
+		6: 'July',
+		7: 'August',
+		8: 'September',
+		9: 'October',
+		10: 'November',
+		11: 'December'
+	};
+	
+	if (isNaN(Number(date))) {
+		
+	} else {
+		// make a module
+		let dateObj = new Date(date * 1000);
+		if (isNaN(dateObj.getFullYear()) || !monthMap[dateObj.getUTCMonth()] || !(date.length == 10)) {
+			res.send(toSend);
+		} else {
+			toSend.unix = dateObj.getTime() / 1000;
+			toSend.natural = monthMap[dateObj.getUTCMonth()] + ' ' + dateObj.getUTCDate() + ', ' + dateObj.getUTCFullYear();
+			res.send(toSend);
+		}
+	}
 });
 
 // handle 404
