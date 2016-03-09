322746d089321bfc7f1bd00fa945397af83fbbb0
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-03-09 19:43:54 +0000

    error and 404 handle

--- app.js
+++ app.js
@@ -10,7 +10,22 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/:date', function(req, res, next) {
-	res.send('TEST');
+	res.send(req.params.date);
+});
+
+// handle 404
+app.use(function(req, res, next) {
+	res.status(404).send('Sorry can\'t find that!');
+});
+
+// error handling
+app.use(function(err, req, res, next) {
+	if (req.xhr) {
+		res.status(500).send({ error: 'Error' });
+	} else {
+		res.send('Error');
+		res.send(err);
+	}
 });
 
 app.listen(8080);
