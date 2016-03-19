12af01d912dc3a90a87fd0fe8a2783388754d3e9
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-03-19 01:13:47 +0000

    Done

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
 'use strict';
 
-const http = require('http');
 const express = require('express');
 const app = express();
+const date = require('./modules/date');
 
 // render
 app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('static'));
 
 app.get('/', function(req, res, next) {
 	res.render('index.html');
 });
 
 app.get('/:date', function(req, res, next) {
-	let date = req.params.date;
+	let dateParam = req.params.date;
 	
-	let toSend = {
-		unix: null,
-		natural: null
-	};
+	let toSend = date(dateParam);
 	
-	let monthMap = {
-		0: 'January',
-		1: 'February',
-		2: 'March',
-		3: 'April',
-		4: 'May',
-		5: 'June',
-		6: 'July',
-		7: 'August',
-		8: 'September',
-		9: 'October',
-		10: 'November',
-		11: 'December'
-	};
-	
-	if (isNaN(Number(date))) {
-		
-	} else {
-		// make a module
-		let dateObj = new Date(date * 1000);
-		if (isNaN(dateObj.getFullYear()) || !monthMap[dateObj.getUTCMonth()] || !(date.length == 10)) {
-			res.send(toSend);
-		} else {
-			toSend.unix = dateObj.getTime() / 1000;
-			toSend.natural = monthMap[dateObj.getUTCMonth()] + ' ' + dateObj.getUTCDate() + ', ' + dateObj.getUTCFullYear();
-			res.send(toSend);
-		}
-	}
+	res.send(toSend);
 });
 
 // handle 404
