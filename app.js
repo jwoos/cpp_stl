cd42a4b67fbda0ec85146f3367ac8ae5b58499c6
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-03-20 15:01:39 +0000

    done

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res, next) {
+    let toSend = {};
+    
+    toSend.ipaddress = req.headers['x-forwarded-for'];
+    toSend.language = req.headers['accept-language'].split(',')[0];
+    toSend.software = req.headers['user-agent'].match(/\(([\w\s;.]+)\)/)[1];
+    console.log(toSend);
+    res.send(toSend);
+});
+
+// handle 404
+app.use(function(req, res, next) {
+	res.status(404).send('Sorry can\'t find that!');
+});
+
+// error handling
+app.use(function(err, req, res, next) {
+	if (req.xhr) {
+		res.status(500).send({ error: 'Error' });
+	} else {
+		res.send('Error');
+		res.send(err);
+	}
+});
+
+app.listen(8080);
