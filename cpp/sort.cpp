0bb84806a5939aee7c8837d02a1dba67426df86d
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-10-03 10:48:21 -0400

    restructured

---
+++ cpp/sort.cpp
@@ -0,0 +1,25 @@
+template <typename T>
+void insertionSort(vector<T>&);
+
+template <typename T>
+void merge(vector<T>&, vector<T>&, int, int, int);
+
+template <typename T>
+void mergeSort(vector<T>&, vector<T>&, int, int);
+
+template <typename T>
+void mergeSort(vector<T>&);
+
+template <typename T>
+void quickSort(vector<T>&, int, int);
+
+template <typename T>
+void selectionSort(vector<T>&);
+
+template <typename T>
+void setSort(vector<T>&);
+
+template <typename T>
+void shellSort(vector<T>&);
+
+#include "sort.cpp"
