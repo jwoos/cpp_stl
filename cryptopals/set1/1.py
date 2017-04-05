3d543cbea0d0b282cca49117347cad0d7528f213
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-04-05 16:58:35 -0400

    moved to jupyter notebooks

--- cryptopals/set1/1.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-import base64
-
-hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-b_array = bytearray.fromhex(hex_string)
-
-print(base64.b64encode(b_array))
