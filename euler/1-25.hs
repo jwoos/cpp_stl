a2f4160f705752f7851b8ee27d39845651fd767a
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-04-25 17:14:43 -0400

    euler | 3

--- euler/1-25.hs
+++ euler/1-25.hs
@@ -10,3 +10,13 @@ sumFibLim :: (Integral a) => (a -> Bool) -> a -> (a, a) -> a
 sumFibLim f lim (x, y) = sum $ takeWhile (<= lim) seq
   where seq = [x | x <- fibonacci, f x]
         fibonacci = x : y : (zipWith (+) fibonacci (tail fibonacci))
+
+-- 3
+isPrimeNaive :: Integer -> Bool
+isPrimeNaive n
+  | length factors == 1 = True
+  | otherwise = False
+  where factors = [f | f <- [1, 3..(floor $ sqrt $ fromIntegral n)], n `mod` f == 0]
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = [x | x <- [1, 3..(floor $ sqrt $ fromIntegral n)], isPrimeNaive x, n `mod` x == 0]
