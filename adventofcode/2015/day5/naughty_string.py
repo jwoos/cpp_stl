950e0f0f82b65efc5027d0809d10dc12a079a07f
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-08-31 17:52:27 +0000

    adventofcode 2015 day5 part 2

--- adventofcode/2015/day5/naughty_string.py
+++ adventofcode/2015/day5/naughty_string.py
@@ -42,7 +42,46 @@ def part1():
     print('Part 1:', len(good_words))
 
 def part2():
-    print('Part 2:')
+    words = open('input.txt').read().strip().splitlines()
+
+    good_words = []
+
+    for word in words:
+        count = {}
+
+        has_between_repeat = False
+        has_double_pair = False
+
+        skip_next = False
+        for i, c in enumerate(word):
+            if i != len(word) - 1 and not skip_next:
+                current_pair = word[i:i + 2]
+
+                if current_pair == current_pair[::-1]:
+                    if i != len(word) - 2 and word[i + 2] == current_pair[0]:
+                        skip_next = True
+
+                current_count = count.get(current_pair)
+                if not current_count:
+                    count[current_pair] = 1
+                else:
+                    count[current_pair] += 1
+            else:
+                skip_next = False
+
+            if i != len(word) - 2 and i != len(word) - 1:
+                current_triplet = word[i:i + 3]
+
+                if current_triplet == current_triplet[::-1]:
+                    has_between_repeat = True
+
+        if len(list(filter(lambda x: x >= 2, count.values()))):
+            has_double_pair = True
+
+        if has_between_repeat and has_double_pair:
+            good_words.append(word)
+
+    print('Part 2:', len(good_words))
 
 if __name__ == '__main__':
     part1()
