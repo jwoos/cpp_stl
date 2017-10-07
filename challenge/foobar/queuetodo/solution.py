0fcf697d3c78e2c650b4c60281e5353b5fd35a91
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-10-07 04:21:51 +0000

    foobar timeout but correct solution

--- challenge/foobar/queuetodo/solution.py
+++ challenge/foobar/queuetodo/solution.py
@@ -1,2 +1,21 @@
+"""
+n XOR 0 = n
+n XOR n = 0
+n XOR (n - d) = d if n and n - d are not powers of two
+"""
+
 def answer(start, length):
-    pass
+    # this times out
+    solution = start
+    go = length
+
+    while go > 0:
+        for i in range(go):
+            solution ^= start + (length * i)
+
+        start += 1
+        go -= 1
+
+    return solution
+
+print(answer(1000000, 100))
