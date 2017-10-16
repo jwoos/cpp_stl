616af8ea9a2149b62c52b8e7a63ae7117637aff3
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-10-16 11:42:27 -0400

    foobar | solved

--- challenge/foobar/preparethebunniesescape/solution.py
+++ challenge/foobar/preparethebunniesescape/solution.py
@@ -3,52 +3,33 @@
 from collections import deque
 
 
-def sum_lists(a, b):
-    return [x + y for x, y in zip(a, b)]
-
 def get_possible_moves(maze, coord):
     max_row = len(maze) - 1
     max_col = len(maze[0]) - 1
 
-    row, col, wb = coord
+    row, col = coord
     moves = []
 
     if row < max_row:
         # no up
-        val = maze[row + 1][col]
-        if not val:
-            moves.append((row + 1, col, wb))
-        elif val and wb:
-            moves.append((row + 1, col, 0))
+        moves.append((row + 1, col))
 
     if row > 0:
         # no down
-        val = maze[row - 1][col]
-        if not val:
-            moves.append((row - 1, col, wb))
-        elif val and wb:
-            moves.append((row - 1, col, 0))
+        moves.append((row - 1, col))
 
     if col < max_col:
         # no left
-        val = maze[row][col + 1]
-        if not val:
-            moves.append((row, col + 1, wb))
-        elif val and wb:
-            moves.append((row, col + 1, 0))
+        moves.append((row, col + 1))
 
     if col > 0:
         # no right
-        val = maze[row][col - 1]
-        if not val:
-            moves.append((row, col - 1, wb))
-        elif val and wb:
-            moves.append((row, col - 1, 0))
+        moves.append((row, col - 1))
 
     return moves
 
-def answer(maze):
-    q = deque([(0, 0, 1)])
+def bfs(maze):
+    q = deque([(0, 0)])
     max_row = len(maze) - 1
     max_col = len(maze[0]) - 1
 
@@ -56,23 +37,59 @@ def answer(maze):
     matrix[0][0] = 1
 
     while q:
-        x, y, wall_break = q.popleft()
+        x, y = q.popleft()
 
         if (x, y) == (max_row, max_col):
-            print(maze)
-            print(matrix)
             return matrix[x][y]
 
-        for m in get_possible_moves(maze, (x, y, wall_break)):
-            m_x, m_y, wb = m
+        for m in get_possible_moves(maze, (x, y)):
+            m_x, m_y = m
 
-            if not matrix[m_x][m_y]:
-                print(m_x, m_y, wb)
+            if not matrix[m_x][m_y] and not maze[m_x][m_y]:
                 matrix[m_x][m_y] = matrix[x][y] + 1
-                q.append((m_x, m_y, wb))
+                q.append((m_x, m_y))
 
     return None
 
+def generate_all_possible(maze):
+    walls = []
+    for x in range(len(maze)):
+        for y in range(len(maze[0])):
+            # passable we don't care
+            if not maze[x][y]:
+                continue
+
+            # get all possible walls
+            moves = get_possible_moves(maze, (x, y))
+            # check of there are less than 3 walls around
+            # which means it can be travelled to and has a route after breaking
+            if sum([maze[i][j] for (i, j) in moves]) < 3:
+                walls.append((x, y))
+
+    # generate all possible mazes when walls are removed
+    all = [maze]
+    for wall in walls:
+        x, y = wall
+        copy = [i[:] for i in maze]
+        copy[x][y] = 0
+        all.append(copy)
+
+    return all
+
+def answer(maze):
+    shortest = 1000000
+
+    all_possible = generate_all_possible(maze)
+
+    for m in all_possible:
+        length = bfs(m)
+
+        if length is not None and length < shortest:
+            shortest = length
+
+    return shortest
+
+# print(answer([[0, 1, 1, 0], [0, 0, 1, 0], [1, 0, 1, 0]]))
 # print(answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]))
 # print(answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]))
 print(answer([
@@ -88,19 +105,5 @@ print(answer([
     [1,1,1,1,1,0,1,0,1,1,0],
     [1,1,1,1,1,0,0,0,1,1,0]
 ]))
-
-[
-    [1, 2, 0, 0, 0, 0, 0, 0, 29, 0, 0],
-    [2, 3, 6, 7, 8, 9, 0, 0, 28, 29, 30],
-    [3, 4, 5, 6, 7, 8, 0, 0, 27, 0, 0],
-    [4, 5, 6, 7, 0, 9, 0, 0, 26, 0, 0],
-    [5, 6, 7, 8, 0, 10, 0, 24, 25, 26, 27],
-    [6, 0, 0, 9, 0, 11, 0, 23, 0, 0, 28],
-    [7, 0, 0, 10, 11, 12, 0, 22, 0, 0, 29],
-    [8, 0, 0, 0, 0, 0, 0, 21, 0, 0, 30],
-    [9, 10, 11, 12, 13, 14, 0, 20, 0, 0, 31],
-    [0, 0, 0, 0, 0, 15, 0, 19, 0, 0, 32],
-    [0, 0, 0, 0, 0, 16, 17, 18, 0, 0, 33]
-]
-
-# print(answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]))
+# print(answer([[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+# print(answer([[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
