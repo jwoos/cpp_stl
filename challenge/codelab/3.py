44f1dfc19892b94a31af2b6e186bf47d49288b4c
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-10-22 18:48:07 -0400

    codelab 3

--- challenge/codelab/3.py
+++ challenge/codelab/3.py
@@ -1,5 +1,5 @@
 """
-Given an array of integers, sort the array into a wave like array and return it, 
+Given an array of integers, sort the array into a wave like array and return it,
 In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
 """
 def wave_sort(l):
@@ -66,25 +66,28 @@ You can only travel in one direction. i to i+1, i+2, ... n-1, 0, 1, 2...
 Completing the circuit means starting at i and ending up at i again.
 """
 def can_complete_circuit(gas, cost):
-    zipped = zip(gas, cost)
-    for i in range(len(zipped)):
-        current = zipped[i:] + zipped[:i]
-        current_gas = 0
-        all = True
+    length = len(gas)
+    current_gas = 0
+    start = 0
+    for i in range(length):
+        current_gas += gas[i] - cost[i]
+        if current_gas < 0:
+            current_gas = 0
+            start = i + 1
 
-        for g, c in current:
-            current_gas += g
-            if current_gas < c:
-                all = False
-                break
-            current_gas -= c
+    if start < length:
+        for i in range(0, start + 1):
+            current_gas += gas[i] - cost[i]
+            if current_gas < 0:
+                return -1
 
-        if all and current_gas >= 0:
-            return i
-
-    return -1
+        return start
+    else:
+        return -1
 
 print can_complete_circuit(
 [959, 329, 987, 951, 942, 410, 282, 376, 581, 507, 546, 299, 564, 114, 474, 163, 953, 481, 337, 395, 679, 21, 335, 846, 878, 961, 663, 413, 610, 937, 32, 831, 239, 899, 659, 718, 738, 7, 209],
 [862, 783, 134, 441, 177, 416, 329, 43, 997, 920, 289, 117, 573, 672, 574, 797, 512, 887, 571, 657, 420, 686, 411, 817, 185, 326, 891, 122, 496, 905, 910, 810, 226, 462, 759, 637, 517, 237, 884]
 )
+
+print can_complete_circuit([0], [0])
